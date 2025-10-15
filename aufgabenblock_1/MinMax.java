19da44debae8b612e7543cd7c986ddf38941ff88
JannikBitzer <bitzer.jannik@googlemail.com>
2025-10-15 20:08:09 +0200

    add Aufgabe 3.2

---
+++ aufgabenblock_1/MinMax.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MinMax {
+
+    private static final int LENGTH = 5;
+
+    public static void main(String[] args) {
+        int[] numbers = new int[LENGTH];
+
+        Scanner scanner = new Scanner(System.in);
+
+        // read and store numbers in array
+        int counter = 0;
+        do {
+            System.out.println("Zahl eingeben: ");
+            numbers[counter] = scanner.nextInt();
+            counter++;
+        } while (counter < numbers.length);
+
+        // find Minimum
+        int min = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        int max = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+
+        scanner.close();
+    }
+}
