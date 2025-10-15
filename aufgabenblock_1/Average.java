04396eeca66c5ad957ec05d98e3394cf0d34a498
JannikBitzer <bitzer.jannik@googlemail.com>
2025-10-15 20:30:03 +0200

    add Aufgabe 3.3

---
+++ aufgabenblock_1/Average.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Average {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wie viele Zahlen solle eingelesen werden? ");
+        int length = scanner.nextInt();
+
+        int[] numbers = new int[length];
+
+        int counter = 0;
+        do {
+            System.out.print(">>> ");
+            numbers[counter] = scanner.nextInt();
+            System.out.print("\n");
+            counter++;
+        } while (counter < length);
+
+        double result = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i];
+        }
+
+        result = result / numbers.length;
+        System.out.println("Durchschnitt: " + result);
+    }
+}
